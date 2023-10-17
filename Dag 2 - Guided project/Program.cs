6554583b5be61afccfc1b62b68403e5c1c54b144
Mape0009 <mape0009@edu.mercantec.dk>
2023-10-17 10:21:12 +0200

    Dag 2 ogaver

--- Dag 2 - Guided project/Program.cs
+++ Dag 2 - Guided project/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 // initialize variables - graded assignments 
+string student;
 int currentAssignments = 5;
+int numberOfPeople = 4;
 
 int sophia1 = 90;
 int sophia2 = 86;
@@ -27,15 +30,19 @@ int logan3 = 87;
 int logan4 = 88;
 int logan5 = 96;
 
+int Class;
+
 int sophiaSum = 0;
 int andrewSum = 0;
 int emmaSum = 0;
 int loganSum = 0;
+int classSum = 0;
 
 decimal sophiaScore;
 decimal andrewScore;
 decimal emmaScore;
 decimal loganScore;
+decimal classScore;
 
 sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
 andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
@@ -46,12 +53,42 @@ sophiaScore = (decimal)sophiaSum / currentAssignments;
 andrewScore = (decimal)andrewSum / currentAssignments;
 emmaScore = (decimal)emmaSum / currentAssignments;
 loganScore = (decimal)loganSum / currentAssignments;
+classScore = (decimal)(sophiaScore + andrewScore + emmaScore + loganScore) / numberOfPeople;
 
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+Console.WriteLine("Which students score would you like to see?");
+Console.WriteLine("Sophia");
+Console.WriteLine("Andrew");
+Console.WriteLine("Emma");
+Console.WriteLine("Logan");
+Console.WriteLine("Class");
 
+student = Console.ReadLine();
+
+if (student == "Sophia")
+{
+    Console.WriteLine("Student\t\tGrade\n");
+    Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+}
+else if (student == "Andrew")
+{
+    Console.WriteLine("Student\t\tGrade\n");
+    Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
+}
+else if (student == "Emma")
+{
+    Console.WriteLine("student\t\tGrade\n");
+    Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
+}
+else if (student == "Logan")
+{
+    Console.WriteLine("Student\t\tGrade\n");
+    Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
+}
+else if (student == "Class")
+{
+    Console.WriteLine("Student\t\tGrade\n");
+    Console.WriteLine("Class:\t\t" + classScore + "\tB+");
+}
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
+
